68a51d0a0a3db1f12c785c7f6bdcb7fca62e65d5
Frederik Mogensen <sodiaf@gmail.com>
2020-12-02 16:57:12 +0100

    Update bootstrap for two monitors, clean unused junk

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "# Cleanup unused junk"
+sudo apt remove --purge scratch2 minecraft-pi wolfram-engine sonic-pi dillo libreoffice libreoffice* squeak-vm squeak-plugins-scratch geany
+sudo apt-get clean
+sudo apt-get autoremove
+
 echo "# Grabbing dependencies"
 sudo apt-get install chromium-browser xorg lightdm x11-xserver-utils unclutter xdotool
 
@@ -12,4 +17,5 @@ ln -fs ${PWD}/xsession ~/.xinitrc
 ln -fs ${PWD}/xsession ~/.xsession
 
 # This is where the list of URLs to cycle through goes
-touch urls.txt
+touch urls-0.txt
+touch urls-1.txt
